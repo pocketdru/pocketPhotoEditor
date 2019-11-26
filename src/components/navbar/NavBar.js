1b20ff48ce6ec49f758f1e183e26acb9024bccc7
pocketdru <pocketdru@gmail.com>
2019-11-26 15:33:52 -0700

    navbar items positioned

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -11,21 +11,18 @@ const NavBar = () => {
   <div className="row">
     <div className="col-md-12">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="collapse navbar-collapse d-flex justify-content-around" id="navbarSupportedContent">
-          <a className="navLink flex-grow-1" href="#">Feed</a>
-          <div className="burst-8 "></div>
-          <a className="navLink" href="#">My Photos</a>
+        <div className="collapse navbar-collapse d-flex justify-content-around row" id="navbarSupportedContent">
+          <a className="navLink col-md-4" href="#">Feed</a>
+          <div className="burst-8 col-md-4"></div>
+          <a className="navLink col-md-3" href="#">My Photos</a>
           {!isAuthenticated && (
-            <a onClick={() => loginWithRedirect({})} className="navLink">Log in</a> )}
-          {isAuthenticated && <a onClick={() => logout()} className="navLink">Log out</a>}
+            <a onClick={() => loginWithRedirect({})} className="navLink login col-md-1">Log in</a> )}
+          {isAuthenticated && <a onClick={() => logout()} className="navLink login col-md-1">Log out</a>}
         </div>
       </nav>
     </div>
   </div>
 </div>
-
-
-
   );
 };
 
