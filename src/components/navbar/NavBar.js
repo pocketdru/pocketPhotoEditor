90695520799ccc222d52390edbcc02b3039c643d
pocketdru <pocketdru@gmail.com>
2019-11-29 10:35:03 -0700

    navbar responsiveness

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -10,12 +10,12 @@ const NavBar = () => {
 console.log(window.innerHeight);
 console.log(window.innerWidth);
 
-const styles = ({
-  // fontSize: window.innerHeight*window.innerWidth*0.01
-  login: {
-    fontSize: 15
-  }
-});
+// const styles = ({
+//   // fontSize: window.innerHeight*window.innerWidth*0.01
+//   login: {
+//     fontSize: 15
+//   }
+// });
 
   return (
 <div className="container">
@@ -23,11 +23,11 @@ const styles = ({
     <div className="col-md-12">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="collapse navbar-collapse d-flex justify-content-around row" id="navbarSupportedContent">
-          <a className="navLink col-md-5" href="#">Feed</a>
+          <a className="navLink col-md-5 feed" href="#">Feed</a>
           <div className="burst-8 col-md-2"></div>
-          <a className="navLink col-md-4 flex-shrink-1" href="#">My Photos</a>
+          <a className="navLink col-md-4 myPhotos" href="#">My Photos</a>
           {!isAuthenticated && (
-            <a onClick={() => loginWithRedirect({})} className="navLink login col-md-1" style={styles.login}>Log in</a> )}
+            <a onClick={() => loginWithRedirect({})} className="navLink login col-md-1">Log in</a> )}
           {isAuthenticated && <a onClick={() => logout()} className="navLink login col-md-1">Log out</a>}
         </div>
       </nav>
