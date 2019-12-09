54333c6f4f4c2cbb948b456f6ee8c85b7de2101e
pocketdru <pocketdru@gmail.com>
2019-12-09 12:38:17 -0700

    welcome slogan added and styled

---
+++ build/precache-manifest.f4e63dc39d813201524cb7d518296282.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1dba9a9e1e5b9e5a5d637c85dc2f8c",
+    "url": "/pocketdru/pocketPhotoEditor/index.html"
+  },
+  {
+    "revision": "33600f29daf71bed7d8d",
+    "url": "/pocketdru/pocketPhotoEditor/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "29b3572a862ffa5deeb0",
+    "url": "/pocketdru/pocketPhotoEditor/static/css/main.77096a3b.chunk.css"
+  },
+  {
+    "revision": "33600f29daf71bed7d8d",
+    "url": "/pocketdru/pocketPhotoEditor/static/js/2.4faec5e2.chunk.js"
+  },
+  {
+    "revision": "29b3572a862ffa5deeb0",
+    "url": "/pocketdru/pocketPhotoEditor/static/js/main.317d01e4.chunk.js"
+  },
+  {
+    "revision": "3150aa994e6698b655f5",
+    "url": "/pocketdru/pocketPhotoEditor/static/js/runtime-main.c43ea106.js"
+  },
+  {
+    "revision": "166a955f64709b02edb44d75f24e5e51",
+    "url": "/pocketdru/pocketPhotoEditor/static/media/Contact_section_BG.166a955f.png"
+  },
+  {
+    "revision": "1b3688d810db89cfbe9568b6e5c73171",
+    "url": "/pocketdru/pocketPhotoEditor/static/media/Header_BG1.1b3688d8.png"
+  },
+  {
+    "revision": "01821753a52f74ab51231452c1246a84",
+    "url": "/pocketdru/pocketPhotoEditor/static/media/Rectangle_1.01821753.png"
+  },
+  {
+    "revision": "91bef03eeb5b31feccf05a65c32d7371",
+    "url": "/pocketdru/pocketPhotoEditor/static/media/Rectangle_4.91bef03e.png"
+  },
+  {
+    "revision": "d751657efd64ccc79aa38213dc74a338",
+    "url": "/pocketdru/pocketPhotoEditor/static/media/Rectangle_4_copy.d751657e.png"
+  }
+]);
