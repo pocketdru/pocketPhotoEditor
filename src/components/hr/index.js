8971b3b4c9aa960f665c7ceb04b980c8da015019
pocketdru <pocketdru@gmail.com>
2019-12-09 10:53:42 -0700

    First commit

--- src/components/hr/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useAuth0 } from "../../react-auth0-spa";
-
-
-const ColoredLine = (props) => (
-console.log(props),
-    <hr className={props.className}/>
-)
-export default ColoredLine;
