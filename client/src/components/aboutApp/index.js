c7ae22a608f6f45b7be2f87c2c3772cfa3c07a82
pocketdru <pocketdru@gmail.com>
2020-02-03 16:33:55 -0700

    about icons

--- client/src/components/aboutApp/index.js
+++ client/src/components/aboutApp/index.js
@@ -7,7 +7,7 @@ const AboutApp = () => {
     return(
         <div className="container about-app">
             <div className="row d-flex justify-content-center">
-                <div className="col-md-8">
+                <div className="col-md-12">
                         <h2 className="aboutApp-h2 mt-4">
                             About App
                         </h2>
@@ -15,6 +15,36 @@ const AboutApp = () => {
                             <img src={Rectangle} className="rectangle"></img>
                             <img src={X} className="x"></img>
                             <img src={Rectangle} className="rectangle"></img>
+                        </div>
+                        <p className="about-desc text-center mt-2">The app gives you multiple opportunities to edit your photos</p>
+
+                </div>
+                <div className="col-md-12">
+                    <div className="card-group">
+                        <div class="card">
+                            <img src="images/about/1.png" className="card-img-top er" alt="..."/>
+                            <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
                         </div>
                 </div>
             </div>
