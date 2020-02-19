76257fb494d19208df32a6c71c5b8b7c5ccb3991
pocketdru <pocketdru@gmail.com>
2020-02-19 00:17:11 -0700

    cards content

--- client/src/components/aboutApp/index.js
+++ client/src/components/aboutApp/index.js
@@ -24,7 +24,8 @@ const AboutApp = () => {
                         <div class="card">
                             <img src="images/about/1.png" className="card-img-top er" alt="..."/>
                             <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title">MOVIES</h5>
+                            <h6>WHAT I WATCH</h6>
                             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                             </div>
