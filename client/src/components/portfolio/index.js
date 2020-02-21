9c39a6a5506a004f2587478c1254066f4b528db5
pocketdru <pocketdru@gmail.com>
2020-02-21 14:38:05 -0700

    labels positioned

--- client/src/components/portfolio/index.js
+++ client/src/components/portfolio/index.js
@@ -2,51 +2,98 @@ import React from "react";
 import "./style.css";
 import X from "../aboutApp/x.png";
 import Rectangle from "../aboutApp/Rectangle_5_copy.png";
-import Tag from "./Tag1.png";
+import Tag from "./Tag.png";
 
 const Portfolio = () => {
     return(
         <div className="container portfolio">
             <div className="row">
                 <div className="col-md-12">
+
+                    {/* Portfolio header and description */}
                     <h2 className="portfolio-h2 mt-4">
                         Portfolio
                     </h2>
                     <div className="d-flex justify-content-center">
-                        <img src={Rectangle} className="rectangle"></img>
-                        <img src={X} className="x"></img>
-                        <img src={Rectangle} className="rectangle"></img>
+                        <img src={Rectangle} className="rectangle" alt="rectangle"></img>
+                        <img src={X} className="x" alt='x'></img>
+                        <img src={Rectangle} className="rectangle" alt="rectangle"></img>
                     </div>
                     <p className="about-desc text-center mt-2">The app gives you multiple opportunities to edit your photos</p>
                 </div>
             </div>
+            {/* Button group */}
             <div className="row mt-4">
                 <div className="col-md-8">
-                    {/* <button className="btn">
-                    
-                    </button> */}
                     <div className="btn-group" role="group" aria-label="Basic example">
-                        <button type="button" className="btn btn-secondary">
+                        <button type="button" className="btn btn-secondary" id="btn">
                         <div className="card">
                             <img src={Tag} className="card-img" alt="..."/>
                             <div className="card-img-overlay">
-                                <p className="card-text">Last updated 3 mins ago</p>
+                                <p className="card-text">design</p>
                             </div>
                         </div>
                         </button>
-                        <button type="button" className="btn btn-secondary">
+                        <button type="button" className="btn btn-secondary"
+                        id="btn">
                         <div className="card">
                             <img src={Tag} className="card-img" alt="..."/>
                             <div className="card-img-overlay">
-                                <p className="card-text">Last updated 3 mins ago</p>
+                                <p className="card-text">design</p>
                             </div>
                         </div>
                         </button>
-                        <button type="button" className="btn btn-secondary">
+                        <button type="button" className="btn btn-secondary"
+                        id="btn">
                         <div className="card">
                             <img src={Tag} className="card-img" alt="..."/>
                             <div className="card-img-overlay">
-                                <p className="card-text">Last updated 3 mins ago</p>
+                                <p className="card-text">design</p>
+                            </div>
+                        </div>
+                        </button>
+                        <button type="button" className="btn btn-secondary"
+                        id="btn">
+                        <div className="card">
+                            <img src={Tag} className="card-img" alt="..."/>
+                            <div className="card-img-overlay">
+                                <p className="card-text">design</p>
+                            </div>
+                        </div>
+                        </button>
+                        <button type="button" className="btn btn-secondary"
+                        id="btn">
+                        <div className="card">
+                            <img src={Tag} className="card-img" alt="..."/>
+                            <div className="card-img-overlay">
+                                <p className="card-text">design</p>
+                            </div>
+                        </div>
+                        </button>
+                        <button type="button" className="btn btn-secondary"
+                        id="btn">
+                        <div className="card">
+                            <img src={Tag} className="card-img" alt="..."/>
+                            <div className="card-img-overlay">
+                                <p className="card-text">design</p>
+                            </div>
+                        </div>
+                        </button>
+                        <button type="button" className="btn btn-secondary"
+                        id="btn">
+                        <div className="card">
+                            <img src={Tag} className="card-img" alt="..."/>
+                            <div className="card-img-overlay">
+                                <p className="card-text">design</p>
+                            </div>
+                        </div>
+                        </button>
+                        <button type="button" className="btn btn-secondary"
+                        id="btn">
+                        <div className="card">
+                            <img src={Tag} className="card-img" alt="..."/>
+                            <div className="card-img-overlay">
+                                <p className="card-text">design</p>
                             </div>
                         </div>
                         </button>
