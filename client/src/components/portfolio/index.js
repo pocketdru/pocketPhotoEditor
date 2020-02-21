df8ad5245d77f618d7e5069b9c06f511d1a974b7
pocketdru <pocketdru@gmail.com>
2020-02-21 12:47:52 -0700

    button group

--- client/src/components/portfolio/index.js
+++ client/src/components/portfolio/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import X from "../aboutApp/x.png";
 import Rectangle from "../aboutApp/Rectangle_5_copy.png";
+import Tag from "./Tag1.png";
 
 const Portfolio = () => {
     return(
@@ -19,12 +20,37 @@ const Portfolio = () => {
                     <p className="about-desc text-center mt-2">The app gives you multiple opportunities to edit your photos</p>
                 </div>
             </div>
-            <div className="row">
+            <div className="row mt-4">
                 <div className="col-md-8">
-                <span className="fa-layers fa-fw">
-                    <i className="fa fa-bookmark" data-fa-transform="rotate--270"></i>
-                    <span className="fa-layers-text fa-inverse">NEW</span>
-                </span>
+                    {/* <button className="btn">
+                    
+                    </button> */}
+                    <div className="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" className="btn btn-secondary">
+                        <div className="card">
+                            <img src={Tag} className="card-img" alt="..."/>
+                            <div className="card-img-overlay">
+                                <p className="card-text">Last updated 3 mins ago</p>
+                            </div>
+                        </div>
+                        </button>
+                        <button type="button" className="btn btn-secondary">
+                        <div className="card">
+                            <img src={Tag} className="card-img" alt="..."/>
+                            <div className="card-img-overlay">
+                                <p className="card-text">Last updated 3 mins ago</p>
+                            </div>
+                        </div>
+                        </button>
+                        <button type="button" className="btn btn-secondary">
+                        <div className="card">
+                            <img src={Tag} className="card-img" alt="..."/>
+                            <div className="card-img-overlay">
+                                <p className="card-text">Last updated 3 mins ago</p>
+                            </div>
+                        </div>
+                        </button>
+                    </div>
                 </div>
                 <div className="col-md-10"></div>
             </div>
