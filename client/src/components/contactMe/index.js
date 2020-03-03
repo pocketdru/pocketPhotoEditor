198acb3cd0b020c94ed954a91fbc845530b5a42f
pocketdru <pocketdru@gmail.com>
2020-03-02 17:31:05 -0700

    contact header

--- client/src/components/contactMe/index.js
+++ client/src/components/contactMe/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./style.css";
-import X from "../aboutApp/x.png";
-import Rectangle from "../aboutApp/Rectangle_5_copy.png";
+import X from "./x.png";
+import Rectangle from "./Rectangle_5_copy.png";
 
 const ContactMe = () => {
     return(
         <div className="container contact">
-
+            <div className="row">
+                <div className="col-md-12">
+                    <h2 className="portfolio-h2 mt-4">
+                        My Blog
+                    </h2>
+                    <div className="d-flex justify-content-center">
+                        <img src={Rectangle} className="rectangle" alt="rectangle"></img>
+                        <img src={X} className="x" alt='x'></img>
+                        <img src={Rectangle} className="rectangle" alt="rectangle"></img>
+                    </div>
+                    <p className="about-desc text-center mt-2">I'LL BE GLAD TO ANSWER YOUR QUESTIONS!</p>
+                </div>
+            </div>
         </div>
     )
 }
