06f9a6477a97951d7532444dee8b8819c64bc90c
pocketdru <pocketdru@gmail.com>
2020-03-11 21:37:11 -0600

    myfeed page created and exported

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar.js";
 import { useAuth0 } from "./react-auth0-spa.js";
 import Home from "./pages/Home";
+import MyFeed from "./pages/MyFeed";
+
 // import { Dimensions } from 'react-native';
 
 
@@ -22,6 +24,7 @@ function App() {
       </header>
       <Switch>
         <Route exact path="/" component={Home}></Route>
+        <Route exact path="/myfeed" component={MyFeed}></Route>
       </Switch>
     </div>
     </Router>
