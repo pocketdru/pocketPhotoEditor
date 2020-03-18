8da4466af560bf494db4498e1ad7fbf3efda0db5
pocketdru <pocketdru@gmail.com>
2020-03-18 14:46:33 -0600

    index models

---
+++ scripts/seedDB.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/hyperlocal"
+);
+
+const userSeed = [{
+    email: "test@test.com",
+    name: "Test"
+  },
+  {
+    email: "hello@test.com",
+    name: "Ima",
+  },
+];
+
+db.User
+  .remove({})
+  .then(() => db.User.collection.insertMany(userSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
