335884dec90e49098a36e28b1582f0d0e0ef4fb4
pocketdru <pocketdru@gmail.com>
2020-03-20 17:12:34 -0600

    upload fixed

--- client/src/components/FileUpload/index.js
+++ client/src/components/FileUpload/index.js
@@ -10,11 +10,10 @@ const FileUpload = () => {
         console.log(e.target.files[0]);
         setFile(e.target.files[0]);
         setFilename(e.target.files[0].name);
-    };
+    }
 
     const onSubmit = async e => {
         e.preventDefault();
-        
         const formData = new FormData();
         formData.append("file", file);
 
@@ -37,18 +36,21 @@ const FileUpload = () => {
 
     return (
         <Fragment>
-           <form>
+           <form onSubmit={onSubmit}>
                 <div className="custom-file">
                 <input type="file" className="custom-file-input" id="customFile" onChange={onChange}/>
                 <label className="custom-file-label" htmlFor="customFile">
                     {filename}
                 </label>
                 </div>
-                <input type="submit" className="btn btn-primary" value="Upload" onClick={onSubmit}/>
+                <input type="submit" 
+                value="Upload"
+                className="btn btn-primary" 
+                />
             </form>
             { uploadedFile ? <div className="row mt-5">
                 <div className="col-md-6 m-auto">
-                    <h3 className="text-center">{ uploadedFile.fileName }</h3>
+                    <h3 className="text-center">{uploadedFile.fileName}</h3>
                     <img style={{width: "100%"}} src={uploadedFile.filePath} alt=""/>
                 </div>
             </div> : null} 
